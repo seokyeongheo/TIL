5e21d6ee3c36c275b373a63f59310e623a798c1e
seokyeongheo <syh602@gmail.com>
2018-08-02 12:27:43 +0900

    recommendation-system-trainning-05

---
+++ 05_recommendation/dss-recommend/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='sy-recommend',
+	packages=find_packages(),
+	include_package_data=True,
+	version="0.0.1",
+	author="SeokyeongHeo",
+	author_email="syh602@gmail.com",
+	zip_safe=False,
+)
